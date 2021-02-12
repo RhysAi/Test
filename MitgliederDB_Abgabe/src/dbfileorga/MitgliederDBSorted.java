649a560038a91088706c36d915e3b23ad57f4d36
RhysAi <78603753+RhysAi@users.noreply.github.com>
2021-02-12 17:49:10 +0100

    Add files via upload

---
+++ MitgliederDB_Abgabe/src/dbfileorga/MitgliederDBSorted.java
@@ -0,0 +1,43 @@
+package dbfileorga;
+
+import java.util.Iterator;
+import static java.lang.Integer.parseInt;
+
+public class MitgliederDBSorted extends MitgliederDB {
+    public MitgliederDBSorted() {
+        super(true);
+    }
+
+    /**
+     * Inserts the record a the specified spot
+     * @param record
+     * @return
+     */
+    @Override
+    public int insert(Record record) {
+        if(record == null) return -1;
+        if (findPos(record.getAttribute(1)) == -1)
+            if (db_operation('i', findInsertPos(record), record) != -1)
+                return findPos(record.getAttribute(1));
+
+        return -1;
+
+    }
+
+    /**
+     * Finds the current position of
+     * @param record
+     * @return
+     */
+    private int findInsertPos(Record record) {
+        Iterator<Record> dbIterator = iterator();
+        int RecNumInt = Integer.parseInt(record.getAttribute(1));
+        String currRecNum;
+        while(dbIterator.hasNext()) {
+            currRecNum = dbIterator.next().getAttribute(1);
+            if( parseInt(currRecNum) > RecNumInt )
+                return findPos(currRecNum);
+        }
+        return this.getNumberOfRecords() + 1;
+    }
+}
---
+++ MitgliederDB_Abgabe/src/dbfileorga/StartMitgliederDBOrdered.java
@@ -0,0 +1,33 @@
+package dbfileorga;
+
+public class StartMitgliederDBOrdered {
+
+	public static void main(String[] args) {
+			MitgliederDB db = new MitgliederDBSorted();
+			
+			System.out.println(db);
+			
+			// TODO test your implementation with the following use cases  
+			
+			// read the a record number e.g. 32 (119;2;44;Albers;Hans;07.10.75;01.05.89;90;25)
+			Record rec = db.read(32);
+			System.out.println(rec);
+
+			//find and read a record with a given Mitgliedesnummer e.g 95 / without binary search
+			rec = db.read(db.findPos("95"));
+			System.out.println(rec);
+
+			//int newRecNum = db.insert(new Record("999;2;44;Meier;Hans;07.05.01;01.03.10;120;15"));
+			//System.out.println(db.read(newRecNum));
+
+			//modify (ID95 Steffi Brahms wird zu ID 95 Steffi Bach)
+			db.modify(db.findPos("125"), new Record("125;3;13;Bach;Steffi;04.04.06;01.02.16;;5"));
+			System.out.println(db.read(db.findPos("95")));
+			
+			//delete the record with Mitgliedsnummer 121 (121;4;;Meller;Peter;24.08.64;01.03.04;230;25) 
+			db.delete(db.findPos("121"));
+			System.out.println(db);
+			
+	}
+
+}
