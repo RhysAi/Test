273b6ad52ebcaeebf4ea488373d6d43d321252d4
RhysAi <78603753+RhysAi@users.noreply.github.com>
2021-02-13 13:57:45 +0100

    Add files via upload

---
+++ MitgliederDB_Abgabe/src/dbfileorga/MitgliederDBUnordered.java
@@ -0,0 +1,23 @@
+package dbfileorga;
+
+public class MitgliederDBUnordered extends MitgliederDB{
+
+    public MitgliederDBUnordered(){
+        super(false);
+    }
+
+    /**
+     * Inserts a Record at the first free space.
+     * @param record
+     * @return
+     */
+    @Override
+    public int insert(Record record) {
+        if (findPos(record.getAttribute(1)) == -1)
+            for (DBBlock dbBlock : db)
+                if (dbBlock.insertRecordAtTheEnd(record) != -1)
+                    return findPos(record.getAttribute(1));
+
+        return -1;
+    }
+}
